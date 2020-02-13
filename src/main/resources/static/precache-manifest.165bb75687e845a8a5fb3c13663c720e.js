d1f19963d3e4242f983745ed93eb733ebf881bee
Aidas Pupelis <aidas.pupelis@asseco.lt>
2020-02-13 11:10:53 +0200

    first commit

---
+++ src/main/resources/static/precache-manifest.165bb75687e845a8a5fb3c13663c720e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d022550a3e25c82a60f8c072400d487f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6af5d4cc147998fcc49a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "20b159f82befabb62228",
+    "url": "/static/css/main.86ded62d.chunk.css"
+  },
+  {
+    "revision": "6af5d4cc147998fcc49a",
+    "url": "/static/js/2.1fc34c8d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.1fc34c8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20b159f82befabb62228",
+    "url": "/static/js/main.417a3e8d.chunk.js"
+  },
+  {
+    "revision": "b47d682f800ac873db0c",
+    "url": "/static/js/runtime-main.04b7d0ad.js"
+  }
+]);
